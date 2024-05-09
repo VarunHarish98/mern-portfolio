26bc4a96407bd61ecc606e9b641f2a83326406f3
Varun Hosadurga Harish <127836353+VarunHarish98@users.noreply.github.com>
2024-05-09 04:18:29 -0400

    Update Education.js

--- my-portfolio/client/src/Components/Education.js
+++ my-portfolio/client/src/Components/Education.js
@@ -18,19 +18,20 @@ const Education = () => {
         <div className='flex flex-col border-l-1 border-tertiary gap-5 sm:flex-row sm:overflow-auto sm:scrollbar-hide sm:gap-7'>
           {education.map((exp, index) => (
             <div className='cursor-pointer'>
-              <h1 key={exp.id} onClick={() => handleClick(index)} className={`${selectedItem === index ? 'text-tertiary border-tertiary -ml-1 bg-[#165232b5] p-2 xl:border-l-4' : 'text-white'}  px-5 sm:px-2 sm:w-[10vh] w-[20vh] text-xl sm:text-lg`}>{exp.degree}</h1>
+              <h1 key={exp.id} onClick={() => handleClick(index)} className={`${selectedItem === index ? 'text-tertiary border-tertiary -ml-1 bg-[#165232b5] p-2 xl:border-l-4' : 'text-white'}  px-5 sm:px-2 sm:w-[15vh] w-[20vh] text-xl sm:text-lg`}>{exp.degree}</h1>
             </div>
           ))}
         </div>
-        <div className='flex sm:flex-col px-20 sm:px-2'>
+        <div className='flex sm:flex-col px-20 sm:px-2 justify-center items-center hover:border-2 hover:rounded-lg hover:border-white hover:p-4'>
           <div className='w-1/3 sm:w-full'>
-            <img className={"border-2 rounded-lg sm:h-52 sm:w-72 h-60 w-72"} src={`${education[selectedItem].image}`} alt={education[selectedItem].title} />
+            <img className={"border-2 rounded-lg sm:h-52 sm:w-72"} src={`${education[selectedItem].image}`} alt={education[selectedItem].title} />
           </div>
-          <div className='text-white w-full px-10 sm:w-full sm:px-2 sm:py-2'>
+          <div className='text-white w-full px-10 sm:w-full sm:px-2 sm:py-4 '>
             <div className=''>
               <p className='text-4xl sm:text-xl text-orange-500 font-semibold'>{education[selectedItem].title}</p>{education[selectedItem].link && <p className='sm:text-lg my-2'><em>Link - {education[selectedItem].link}</em></p>}
-              <p className='my-2'>Major in {education[selectedItem].major}</p>
+              <p className='my-2'>{education[selectedItem].major}</p>
               <p className='my-2'>{education[selectedItem].year}</p>
+              {/* <p className='my-2'>{education[selectedItem].courses}</p> */}
             </div>
           </div>
         </div>
