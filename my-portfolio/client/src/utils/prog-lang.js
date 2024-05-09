aed4f980375a6d82d6241e4e91f1658efb50acaf
Varun Hosadurga Harish <127836353+VarunHarish98@users.noreply.github.com>
2024-05-09 04:14:34 -0400

    Create prog-lang.js

---
+++ my-portfolio/client/src/utils/prog-lang.js
@@ -0,0 +1,16 @@
+import { faAws, faCss3, faHtml5, faJava, faJs, faNode, faNodeJs, faPython, faReact } from '@fortawesome/free-brands-svg-icons'
+import { faCube, faDatabase } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+export let progLanguages = {
+    "Javascript": faJs,
+    "React": faReact,
+    "Java": faJava,
+    "HTML": faHtml5,
+    "CSS": faCss3,
+    "Redis": faCube,
+    "NodeJS": faNode,
+    "Express.js": faNodeJs,
+    "AWS": faAws,
+    "Python": faPython,
+    "MongoDB": faDatabase
+}
