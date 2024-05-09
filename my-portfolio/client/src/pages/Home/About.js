c180ab15a7e43cf09c64c7cc65937d9c41197885
Varun Hosadurga Harish <127836353+VarunHarish98@users.noreply.github.com>
2024-05-09 04:16:08 -0400

    Update About.js

--- my-portfolio/client/src/pages/Home/About.js
+++ my-portfolio/client/src/pages/Home/About.js
@@ -2,11 +2,15 @@ import React from 'react'
 import SectionTitle from '../../Components/SectionTitle'
 import Button from '../../Components/Button'
 import { useSelector } from 'react-redux'
+import { progLanguages } from '../../utils/prog-lang'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+let Tooltip = require('rc-tooltip');
+
 
 const About = () => {
     const { portfolioData } = useSelector(state => state.root)
-    const {about} = portfolioData
-    const {url, description_first, description_last, techVerbiage, skills} = about
+    const { about } = portfolioData
+    const { url, description_first, description_last, techVerbiage, skills } = about
     return (
         <div>
             <SectionTitle title={"About Me"} />
@@ -27,11 +31,25 @@ const About = () => {
                 <div className='text-tertiary text-2xl'>
                     {techVerbiage}
                 </div >
-                <div className='flex flex-wrap gap-4 mt-5'>
-                    {skills.map((item, index) =>
-                        <Button key={index} title={item} margin={'mx-4 sm:mx-2'} padding={'sm:px-3 px-4'} height={"h-12"} color={"text-tertiary"} type={"rounded-lg"} border={"border-2"} others={"border-tertiary"} />
-                    )}
+                <div className='gap-4 flex rounded-lg py-10'>
+                    {skills.map((item) => (
+                        <div
+                            key={item}
+                            className="flex justify-center hover:border-0 items-center hover:shadow hover:border-lg relative"
+                        >
+                            <FontAwesomeIcon
+                                title={item}
+                                color='white'
+                                icon={progLanguages[item.replace(" ", "")]}
+                                className="w-10 h-10 mr-2 hover:border-2 rounded-lg cursor-pointer border-white p-2 shadow"
+                            />
+                            <span className="absolute bg-black px-2 py-1 rounded-md bottom-full left-1/2 transform -translate-x-1/2 opacity-0 transition-opacity ">
+                                {item}
+                            </span>
+                        </div>
+                    ))}
                 </div>
+
             </div>
         </div>
     )
